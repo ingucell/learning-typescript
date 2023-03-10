967b203aa2fe5367b9e4ef58b2ac8fb002642322
ingucell <mathiaslawson70@gmail.com>
2023-03-10 15:23:57 +0000

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let id = 5; //setting type of variable to number
+console.log(id);
+// tsc --watch index -- to automatically compile on changes
+//tsc --init -- to create typescript configuration file
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let id: number = 5   //setting type of variable to number
+
+
+console.log(id)
+// tsc --watch index -- to automatically compile on changes
+//tsc --init -- to create typescript configuration file

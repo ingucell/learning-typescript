b3c67b00fa214a21040a4b30478f5d33be758031
Mathias Prince Lawson <73763892+ingucell@users.noreply.github.com>
2024-01-03 00:52:23 +0000

    module infomation and declarations

---
+++ src/NewVersion/index.js
@@ -0,0 +1,7 @@
+console.log("may name is mathias");
+var num = 5; //you have to declare data type after using variables;
+//:any -- for any data type.
+//tsc index.ts - to compile file
+//how code conversion occurs and how to make restrictions
+var age = 20;
+console.log(age);
---
+++ src/NewVersion/index.ts
@@ -0,0 +1,14 @@
+export {}; // it serves as a signal to TypeScript that the file should be treated as a module, even if it doesn't have any exports. This is often used in situations where you have a file that contains only type declarations or utility functions and 
+
+console.log("may name is mathias")
+
+let num: number = 5 ;  //you have to declare data type after using variables;
+//:any -- for any data type.
+
+//tsc index.ts - to compile file
+
+
+//how code conversion occurs and how to make restrictions
+
+let age:number = 20;
+console.log(age)
